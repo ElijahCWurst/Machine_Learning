182be995d9663196e434193aa9716a0c09ce67ae
ElijahWurst <elijahwurst@gmail.com>
2024-03-06 09:55:29 -0700

    ITS MAKING SENSE

--- Decision_Tree/Node.py
+++ Decision_Tree/Node.py
@@ -1,7 +1,7 @@
-class Node():
-    def __init__(self, feature_index=None, threshold=None, left=None, right=None, value=None):
-        self.feature_index = feature_index
-        self.threshold = threshold
-        self.left = left
-        self.right = right
-        self.value = value
+class Node:
+	def __init__(self, feature=None, value=None, results=None, true_branch=None, false_branch=None):
+		self.feature = feature # Feature to split on
+		self.value = value	 # Value of the feature to split on
+		self.results = results # Stores class labels if node is a leaf node
+		self.true_branch = true_branch # Branch for values that are True for the feature
+		self.false_branch = false_branch # Branch for values that are False for the feature
--- Decision_Tree/main.py
+++ Decision_Tree/main.py
@@ -1,29 +1,86 @@
 import numpy as np
 import pandas as pd
 
-from sklearn import tree
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+# calculate information gain and return the best feature to split on
+
+def loadData(filepath: str):
+    attributeCount = 0
+    attributes: list[list[str]] = []
+    labels: list[str] = []
+    data: list[list[str]] = []
+
+    for i, line in enumerate(open(filepath)):
+        if(i == 0):
+            attributeCount = int(line)
+        elif(i <= attributeCount):
+            attributes.append(line.split())
+        elif(i == attributeCount + 1):
+            labels = line.split()
+            labels.pop(0)
+        else:
+            data.append(line.split())
+
+    return attributes, labels, data
+
+def infoD(targets: list[int], labels) -> float:
+    # count how many times each label appears in the targets
+    counts = []
+    product: float = 0
+    for i, label in enumerate(labels):
+        i = 0
+        for target in targets:
+            if label == target:
+                i += 1
+        counts.append(i)
+    print(counts[0])
+    print(counts[1])
+    for count in counts:
+        if(len(targets) == 0):
+            product += 0
+        else:
+            product += (-1 * (count / len(targets)) * np.log2(count / len(targets)))
+    return product
+
+def infoAttributeD(targets: list[int], inputs, attributes, labels) -> float:
+    counts = []
+    indexer = 0
+    for i, attribute in enumerate(attributes):
+        for j, value in enumerate(attribute):
+            for k, target in enumerate(targets):
+                if value == inputs[k, i]:
+                    indexer += 1
+            counts.append(indexer)
+            indexer = 0
+        counts.pop(0)
+        print(attribute)
+        print(inputs[:, i])
+        print(counts)
+        counts.clear()
+    return -1
+
+
+
 
 filepath = './Decision_Tree/testDataA4/golf.in'
 
-# print(attributeCount)
-attributes: list[list[str]] = []
-labels: list[str] = []
-data: list[list[str]] = []
+attributes, labels, trainingdatastr = loadData(filepath)
 
+trainingdata: np.ndarray[int, np.dtype[np.int_]] = np.array(trainingdatastr)
 
-for i, line in enumerate(open(filepath)):
-    if(i == 0):
-        attributeCount = int(line)
-    elif(i <= attributeCount):
-        attributes.append(line.split())
-    elif(i == attributeCount + 1):
-        labels = line.split()
-        labels.pop(0)
-    else:
-        data.append(line.split())
+inputs = trainingdata[:, :-1]
+targetsNP = trainingdata[:, -1]
 
-print(pd.DataFrame(attributes))
-print(pd.DataFrame(labels))
-print(pd.DataFrame(data))
+targetsList = targetsNP.tolist()
+
+# remove the first column from attributes
+# attributes = [attr[1:] for attr in attributes]
+
+# print(infoD(targetsList, labels))
+infoAttributeD(targetsList, inputs, attributes, labels)
+
+# print(pd.DataFrame(trainingdata))
+# print(pd.DataFrame(inputs))
+# print(pd.DataFrame(targetsNP))
+# print(pd.DataFrame(attributes))
+# print(pd.DataFrame(labels))
+
