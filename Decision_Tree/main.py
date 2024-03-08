df0b2bdbbf1e21c53a66c8d8688684fb7ec7a65c
ElijahWurst <elijahwurst@gmail.com>
2024-03-07 21:57:47 -0700

    almost done

--- Decision_Tree/main.py
+++ Decision_Tree/main.py
@@ -7,7 +7,7 @@ getcontext().prec = 60
 # calculate information gain and return the best feature to split on
 
 def main():
-    filepath = './Decision_Tree/testDataA4/continue2.in'
+    filepath = './Decision_Tree/testDataA4/golfc.in'
 
     attributes, labels, trainingdatastr = loadData(filepath)
 
@@ -18,11 +18,13 @@ def main():
 
     targetsList = targetsNP.tolist()
 
+    usedSplits = []
+
     # bestSplitAttribute = infoAttributeD(targetsList, inputs, attributes, labels)
     # print(bestSplitAttribute)
 
 
-    generateDecisionTree(targetsList, inputs, attributes, labels)
+    generateDecisionTree(targetsList, inputs, attributes, labels, usedSplits)
 
 
     # print(pd.DataFrame(trainingdata))
@@ -68,7 +70,7 @@ def infoD(targets: list[int], labels) -> float:
             product += (-1 * (count / len(targets)) * np.log2(count / len(targets)))
     return product
 
-def infoAttributeD(targets, inputs, attributes, labels):
+def infoAttributeD(targets, inputs, attributes, labels, usedSplits):
     continuous = False
     continuousAttributes = []
     continuousIndices = []
@@ -78,8 +80,13 @@ def infoAttributeD(targets, inputs, attributes, labels):
     partitionedCountsLT = []
     partitionedCountsGT = []
     bestSplitAttributeIndex: int = -1
+    bestContSplitAttributeIndex: int = -1
     bestSplitAttribute: str = ''
+    bestContSplitAttribute: str = ''
+    bestContSplit: float = -1
     currentInfoGain = Decimal(0)
+    currentContInfoGain = Decimal(0)
+    bestContInfoGain = Decimal(-1)
     bestInfoGain = Decimal(-1)
     targetedCounts = []
     indexer = 0
@@ -91,6 +98,9 @@ def infoAttributeD(targets, inputs, attributes, labels):
     product = Decimal(0)
     possibleSplits = []
     alreadyCheckedLT = False
+    infod = Decimal(infoD(targets, labels))
+    usedSplit = ''
+
     
     # Attribute is the line in the file, ex: ['age', '0-30', '31-60', '61-100']
     # Value is the value of the attribute, ex: '0-30', and we skip the first value in the attribute list 
@@ -100,6 +110,7 @@ def infoAttributeD(targets, inputs, attributes, labels):
     #       target loop is for that.
     for i, attribute in enumerate(attributes):
         alreadyCheckedLT = False
+        continuous = False
         for j, value in enumerate(attribute):
             if(j == 0):
                 continue
@@ -110,41 +121,30 @@ def infoAttributeD(targets, inputs, attributes, labels):
                 continuousColumn = inputs[:, i]
                 continuousColumn = np.sort(continuousColumn)
                 continuousColumn = np.unique(continuousColumn)
-                attribute.pop(1)
+                # calculate possible splits
+                for k in range(len(continuousColumn) - 1):
+                    possibleSplits.append((float(continuousColumn[k]) + float(continuousColumn[k+1])) / 2)
 
-                # Get the split value options from the continuous attribute and add them to the attributes list
-                for m in range(len(continuousColumn) - 1):
-                    if m == 0:
-                        # edit current attribute
-                        attribute.append(str((float(continuousColumn[m]) + float(continuousColumn[m+1])) / 2))
-                        attribute.append(str((float(continuousColumn[m]) + float(continuousColumn[m+1])) / 2))
-                        # duplicate the current attribute's column in the inputs array
-                        inputs = np.insert(inputs, i, inputs[:, i], axis=1)
-                    else:
-                        # add new attribute
-                        attributes.append([attribute[0], str((float(continuousColumn[m]) + float(continuousColumn[m+1])) / 2), str((float(continuousColumn[m]) + float(continuousColumn[m+1])) / 2)])
-                value = attribute[j]
-                
 
             for k, target in enumerate(targets):
                 # If the attribute is continuous, we need to check <= and > for the value of the attribute in seperate iterations
-                if continuous:
-                    if alreadyCheckedLT:
-                        if float(inputs[k, i]) > float(value):
-                            indexer += 1
-                            if target == labels[0]:
-                                indexer2 += 1
-                    elif float(inputs[k, i]) <= float(value):
+                # if continuous:
+                #     if alreadyCheckedLT:
+                #         if float(inputs[k, i]) > float(value):
+                #             indexer += 1
+                #             if target == labels[0]:
+                #                 indexer2 += 1
+                #     elif float(inputs[k, i]) <= float(value):
 
-                        indexer += 1
-                        if target == labels[0]:
-                            indexer2 += 1
-                elif value == inputs[k, i]:
+                #         indexer += 1
+                #         if target == labels[0]:
+                #             indexer2 += 1
+                if value == inputs[k, i]:
                     indexer += 1
                     if target == labels[0]:
                         indexer2 += 1
-            if continuous:
-                alreadyCheckedLT = True
+            # if continuous:
+            #     alreadyCheckedLT = True
 
             # Store the amount of 'yes's and 'no's for each value of the attribute and reset the counters.
             targetedCounts.append(indexer2)            
@@ -153,53 +153,76 @@ def infoAttributeD(targets, inputs, attributes, labels):
             indexer2 = 0
 
         # If the attribute is continuous, we need to calculate the information gain differently
-        # if(continuous):
+        if(continuous):
+            for split in possibleSplits:
+                if usedSplits != None:
+                    if ((attribute[0] + str(split)) in usedSplits):
+                        continue
+                for n, target in enumerate(targets):
+                    if float(inputs[n, i]) < split:
+                        indexerLT += 1
+                        if target == labels[0]:
+                            indexer2LT += 1
+                    elif float(inputs[n, i]) >= split:
+                        indexerGT += 1
+                        if target == labels[0]:
+                            indexer2GT += 1
+                countsLT.append(indexerLT)
+                countsGT.append(indexerGT)
+                partitionedCountsLT.append(indexer2LT)
+                partitionedCountsGT.append(indexer2GT)
+                indexerLT = 0
+                indexerGT = 0
+                indexer2LT = 0
+                indexer2GT = 0
 
-            # for split in possibleSplits:
-            #     for n, target in enumerate(targets):
-            #         if float(inputs[n, i]) < split:
-            #             indexerLT += 1
-            #             if target == labels[0]:
-            #                 indexer2LT += 1
-            #         elif float(inputs[n, i]) >= split:
-            #             indexerGT += 1
-            #             if target == labels[0]:
-            #                 indexer2GT += 1
-            #     countsLT.append(indexerLT)
-            #     countsGT.append(indexerGT)
-            #     partitionedCountsLT.append(indexer2LT)
-            #     partitionedCountsGT.append(indexer2GT)
-            #     indexerLT = 0
-            #     indexerGT = 0
-            #     indexer2LT = 0
-            #     indexer2GT = 0
+                if(len(targets) == 0):
+                    product += 0
+                elif (countsLT[0] - partitionedCountsLT[0] == 0 or partitionedCountsLT[0] == 0):
+                    product += 0
+                elif (countsGT[0] - partitionedCountsGT[0] == 0 or partitionedCountsGT[0] == 0):
+                    product += 0
+                else:
+                    product += ((Decimal(((countsLT[0]/len(targets)) * (-1 * ((partitionedCountsLT[0] / countsLT[0]) * np.log2(partitionedCountsLT[0] / countsLT[0]) + (((countsLT[0] - partitionedCountsLT[0]) / countsLT[0]) * np.log2((countsLT[0] - partitionedCountsLT[0]) / countsLT[0])))))))
+                             + (Decimal(((countsGT[0]/len(targets)) * (-1 * ((partitionedCountsGT[0] / countsGT[0]) * np.log2(partitionedCountsGT[0] / countsGT[0]) + (((countsGT[0] - partitionedCountsGT[0]) / countsGT[0]) * np.log2((countsGT[0] - partitionedCountsGT[0]) / countsGT[0]))))))))
 
-            # for l, count in enumerate(countsLT):
+                currentContInfoGain = infod - product
+                if(currentContInfoGain > bestContInfoGain):
+                    bestContInfoGain = currentContInfoGain
+                    bestContSplitAttribute = attribute[0]
+                    bestContSplitAttributeIndex = i
+                    bestContSplit = split
+        else:
+            # The math that actually calculates the information gain
+            for l, count in enumerate(counts):
+                if(len(targets) == 0):
+                    product += 0
+                elif (count - targetedCounts[l] == 0 or targetedCounts[l] == 0):
+                    product += 0
+                else:
+                    product = (product + Decimal(((count/len(targets)) * (-1 * ((targetedCounts[l] / count) * np.log2(targetedCounts[l] / count) + (((count - targetedCounts[l]) / count) * np.log2((count - targetedCounts[l]) / count)))))))
+            
+            currentInfoGain = infod - product
+            if(currentInfoGain > bestInfoGain):
+                bestInfoGain = currentInfoGain
+                bestSplitAttribute = attribute[0]
+                bestSplitAttributeIndex = i
 
-
-        # The math that actually calculates the information gain
-        for l, count in enumerate(counts):
-            if(len(targets) == 0):
-                product += 0
-            elif (count - targetedCounts[l] == 0 or targetedCounts[l] == 0):
-                product += 0
-            else:
-                product = (product + Decimal(((count/len(targets)) * (-1 * ((targetedCounts[l] / count) * np.log2(targetedCounts[l] / count) + (((count - targetedCounts[l]) / count) * np.log2((count - targetedCounts[l]) / count)))))))
+        if bestContInfoGain > bestInfoGain:
+            bestInfoGain = bestContInfoGain
+            bestSplitAttribute = bestContSplitAttribute
+            bestSplitAttributeIndex = bestContSplitAttributeIndex
+            # usedSplits.append(bestContSplitAttribute + str(bestContSplit))
+        else:
+            continuous = False
         
-        infod = Decimal(infoD(targets, labels))
-        currentInfoGain = infod - product
-        if(currentInfoGain > bestInfoGain):
-            bestInfoGain = currentInfoGain
-            bestSplitAttribute = attribute[0]
-            bestSplitAttributeIndex = i
-
         product = 0
         targetedCounts.clear()
         counts.clear()
 
-    return bestSplitAttribute, bestSplitAttributeIndex, targets, inputs, attributes
+    return bestSplitAttribute, bestSplitAttributeIndex, continuous, usedSplit, bestContSplit
 
-def splitOnAttribute(splitAttribute, bestSplitIndex, targets, inputs, attributes):
+def splitOnAttribute(splitAttribute, bestSplitIndex, targets, inputs, attributes, usedSplits):
     splitInput = []
     splitTarget = []
     splitInputList = []
@@ -214,19 +237,23 @@ def splitOnAttribute(splitAttribute, bestSplitIndex, targets, inputs, attributes
         if j == 0:
             continue
         for i, input in enumerate(inputs):
-            # if attribute[1] == attribute[2]: we are dealing with a continuous attribute
-            if attributes[splitAttributeIndex][1] == attributes[splitAttributeIndex][2]:
-                # the first index indicates this is the less than or equal to split
-                if j == 1:
-                    if float(input[splitAttributeIndex]) <= float(attributes[splitAttributeIndex][1]):
-                        splitInput.append(input)
-                        splitTarget.append(targets[i])
-                # the second index indicates this is the greater than split
-                elif j == 2:
-                    if float(input[splitAttributeIndex]) > float(attributes[splitAttributeIndex][1]):
-                        splitInput.append(input)
-                        splitTarget.append(targets[i])
-            elif input[splitAttributeIndex] == attributes[splitAttributeIndex][j]:
+
+
+            # # if attribute[1] == attribute[2]: we are dealing with a continuous attribute
+            # if attributes[splitAttributeIndex][1] == attributes[splitAttributeIndex][2]:
+            #     # the first index indicates this is the less than or equal to split
+            #     if j == 1:
+            #         if float(input[splitAttributeIndex]) <= float(attributes[splitAttributeIndex][1]):
+            #             splitInput.append(input)
+            #             splitTarget.append(targets[i])
+            #     # the second index indicates this is the greater than split
+            #     elif j == 2:
+            #         if float(input[splitAttributeIndex]) > float(attributes[splitAttributeIndex][1]):
+            #             splitInput.append(input)
+            #             splitTarget.append(targets[i])
+
+
+            if input[splitAttributeIndex] == attributes[splitAttributeIndex][j]:
                 splitInput.append(input)
                 splitTarget.append(targets[i])
         splitInputList.append(np.array(splitInput))
@@ -245,7 +272,35 @@ def splitOnAttribute(splitAttribute, bestSplitIndex, targets, inputs, attributes
         
     return splitInputList, splitTargetList, newAttributes
 
-def generateDecisionTree(targets, inputs, attributes, labels, depth=0):
+def splitOnContinuousAttribute(splitAttribute, bestSplitIndex, targets, inputs, attributes, usedSplits, bestContSplitValue):
+    splitInput = []
+    splitTarget = []
+    splitInputList = []
+    splitTargetList = []
+    splitAttributeIndex = bestSplitIndex
+
+    for j in range (3):
+        if j == 0:
+            continue
+        for i, input in enumerate(inputs):
+            if j == 1:
+                if float(input[splitAttributeIndex]) <= bestContSplitValue:
+                    splitInput.append(input)
+                    splitTarget.append(targets[i])
+            elif j == 2:
+                if float(input[splitAttributeIndex]) > bestContSplitValue:
+                    splitInput.append(input)
+                    splitTarget.append(targets[i])
+        splitInputList.append(np.array(splitInput))
+        splitTargetList.append(np.array(splitTarget))
+        splitInput.clear()
+        splitTarget.clear()
+    
+    newAttributes = attributes.copy()
+        
+    return splitInputList, splitTargetList, newAttributes
+
+def generateDecisionTree(targets, inputs, attributes, labels, usedSplits, depth=0):
     
     rowCount = len(inputs)
 
@@ -267,13 +322,22 @@ def generateDecisionTree(targets, inputs, attributes, labels, depth=0):
         print("\t" * (depth) + labels[labelCount.index(max(labelCount))])
         return
 
-    bestSplit, bestSplitIndex, tempTargets, tempInputs, tempAttributes = infoAttributeD(targets, inputs, attributes, labels)
-    newInputs, newTargets, newAttributes = splitOnAttribute(bestSplit, bestSplitIndex, tempTargets, tempInputs, tempAttributes)
+    bestSplit, bestSplitIndex, isContinuous, newUsedSplit, bestContSplitValue = infoAttributeD(targets, inputs, attributes, labels, usedSplits)
+    if isContinuous:
+        newInputs, newTargets, newAttributes = splitOnContinuousAttribute(bestSplit, bestSplitIndex, targets, inputs, attributes, newUsedSplit, bestContSplitValue)
+    else:
+        newInputs, newTargets, newAttributes = splitOnAttribute(bestSplit, bestSplitIndex, targets, inputs, attributes, usedSplits)
 
     for i, input in enumerate(newInputs):
-        print("\t" * (depth) + bestSplit + " = " + attributes[bestSplitIndex][i+1] + ":")
-
-        generateDecisionTree(newTargets[i], input, newAttributes, labels, depth+1)
+        if isContinuous:
+            if i == 0:
+                print("\t" * (depth) + bestSplit + " <= " + str(round(bestContSplitValue, 2)) + ":")
+            else:
+                print("\t" * (depth) + bestSplit + " > " + str(round(bestContSplitValue, 2)) + ":")
+        else:
+            print("\t" * (depth) + bestSplit + " = " + attributes[bestSplitIndex][i+1] + ":")
+        usedSplits.append(newUsedSplit)
+        generateDecisionTree(newTargets[i], input, newAttributes, labels,  usedSplits, depth+1)
 
 
 
